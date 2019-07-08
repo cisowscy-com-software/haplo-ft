df818881f621398b6fb0bb92365fe98a40bb7f67
Cisowscy.com Jan Roman Cisowski <janromancisowski@cisowscy.com>
2019-07-09 01:11:57 +0200

    WTF not working

--- apps/haplo-ft/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'haplo-ft',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/haplo-ft',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
