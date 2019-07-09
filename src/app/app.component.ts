03737996800ec936d49492dc95a3c5c3e466a944
Jan Roman Cisowski <janromancisowski@cisowscy.com>
2019-07-09 03:17:05 +0200

    ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild, ViewChildren, OnInit, ElementRef, NgModule, Renderer2 } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-})
-export class AppComponent  {
-  name = 'Angular';
-}
+    selector: 'my-app',
+    templateUrl: '../views/root.html',
+    styleUrls: ['../views/root.scss'],
+    encapsulation: ViewEncapsulation.None
+  })
+
+  export class AppComponent {
+    showControlPanel:boolean = true;
+
+   
+
+  }
